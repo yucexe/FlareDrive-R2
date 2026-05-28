83d407f9194199c92e02b00408c0e6d4ce448442
yucexe <140894424+yucexe@users.noreply.github.com>
2026-05-28 04:33:57 -0500

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -20,9 +20,9 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
+      <strong>项目:</strong><a href="https://github.com/yucexe/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>作者:</strong><a href="https://github.com/yucexe" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
       <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
@@ -40,10 +40,10 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
-      emailUrl: "mailto:01@liushen.fun"
+      homeUrl: "https://xiguang.hidns.vip/about/",
+      blogUrl: "https://xiguang.hidns.vip",
+      githubUrl: "https://github.com/yucexe",
+      emailUrl: "mailto:opporin@outlook.com"
     };
   }
 };
